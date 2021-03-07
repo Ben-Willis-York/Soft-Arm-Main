19f524dba4fc8b2e52bc8f3b38d810ec3c4a5e80
Ben <bcw515@york.ac.uk>
2021-03-07 19:00:30 +0000

    Adding Control Panel Class

---
+++ scripts/controlPanel.py
@@ -0,0 +1,15 @@
+import display, robot, math, time, VectorClass, simulator, controller
+from Tkinter import *
+
+import tf2_py
+import tf2_ros
+import rospy, os, time
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+import geometry_msgs.msg
+
+import roslib; roslib.load_manifest('urdfdom_py')
+from urdf_parser_py.urdf import URDF
+
+
+class ControlPannel()
