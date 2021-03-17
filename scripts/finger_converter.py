19c5739c99829f005e74a85142f23529fe53c0e8
Ben <bcw515@york.ac.uk>
2021-03-17 14:16:16 +0000

    Gripper position estimation
    rough estimation from stess/strain sensors

--- scripts/finger_converter.py
+++ scripts/finger_converter.py
@@ -3,11 +3,22 @@
 
 import xml.etree.ElementTree as ET
 import os, sys
-
+from VectorClass import *
 #file = open("Design.xacro", "r")
 
 path = os.path.dirname(os.path.dirname(__file__))
 
+def generateSpringYAML(name, jointNames):
+    string = """type: "effort_controllers/JointGroupPositionController"\njoints:"""
+    for j in jointNames:
+        string += "\n - " + j 
+
+    for j in jointNames:
+        string += "\n"+j+":\n pid: {p: -0.20, i: 0.0, d: 0.0, i_clamp_min : -100, i_clamp_max : 100}"
+
+    file = open(path+"/config/"+name+".yaml", "w")
+    file.write(string)
+    file.close()
 
 
 
@@ -38,7 +49,9 @@ def convertFingers(root):
             length=None
             origin=None
             rotation=None
-
+            baseGeom=None
+            tipGeom=None
+            baseForce=None 
 
             try:
                 for param in child:
@@ -57,6 +70,16 @@ def convertFingers(root):
                             rotation=param.attrib["rpy"]
                         except:
                             rotation="0 0 0"
+                    if(param.tag=="geometry"):
+                        nums = param.attrib["base"].split()
+                        baseGeom = Vector2(float(nums[0]), float(nums[1]))
+                        print(baseGeom)
+                        nums = param.attrib["tip"].split()
+                        tipGeom = Vector2(float(nums[0]), float(nums[1]))
+                        print(tipGeom)
+                    if(param.tag=="force"):
+                        baseForce = float(param.attrib["value"])
+
             except:
                 print("MISSING PARAMETERS FOR FINGER")
                 break
@@ -68,19 +91,28 @@ def convertFingers(root):
             lengthString = '"%s"' % (leng)
 
             addedLines.append("\n <!--  MACROS FOR FINGER: " + name + "-->\n")
-            addedLines.append("\n<xacro:seg name=" + ('"%s"' % (name+"seg1")) + " length="+lengthString+"/>\n")
-            addedLines.append("<xacro:seg_joint_offset seg1=" + ('"%s"' % (base)) + " seg2=" + ('"%s"' % (name+"seg1")) + " length="+ lengthString + " offset=" + ('"%s"' % (origin)) +  " rotation=" +  ('"%s"' % (rotation)) + " />\n")
+            addedLines.append('\n<xacro:seg name= "%s" length = "%s" height = "%s" width = "%s" />\n' % (name+"seg1", leng, str(baseGeom.y), str(baseGeom.x))) 
+            #addedLines.append("\n<xacro:seg name=" + ('"%s"' % (name+"seg1")) + " length="+lengthString+" height="+str(baseGeom.y)+" width="+str(baseGeom.x)+"/>\n")
+            addedLines.append('<xacro:seg_joint_offset seg1= "%s" seg2="%s" length="%s" offset="%s" rotation="%s" force="%s" />\n' % (base, name+"seg1", leng, origin, rotation, baseForce))
+            #addedLines.append("<xacro:seg_joint_offset seg1=" + ('"%s"' % (base)) + " seg2=" + ('"%s"' % (name+"seg1")) + " length="+ lengthString + " offset=" + ('"%s"' % (origin)) +  " rotation=" +  ('"%s"' % (rotation)) + " />\n")
             addedLines.append("<xacro:seg_trans seg1=" + ('"%s"' % (base)) + " seg2="+ ('"%s"' % (name+"seg1")) + "/>\n")
             
             
             jointNames.append(base+"_"+name+"seg1_joint")
+            interval = (baseGeom-tipGeom)/segments
+            baseArea = baseGeom.x * baseGeom.y
             for n in range(1,segments):
-                segName = '"%s"' % (name+"seg" +str(n+1))
-                parentName = '"%s"' % (name+"seg" +str(n))
-                addedLines.append("<xacro:segment name=" +segName+" parent="+parentName + " length="+lengthString+"/>\n")
+                geom = baseGeom - (interval*n)
+                area = geom.x * geom.y
+                force = baseForce * (area/baseArea)
+                segName = name+"seg" +str(n+1)
+                parentName = name+"seg" +str(n)
+                addedLines.append('<xacro:segment name= "%s" parent="%s" length="%s" height="%s" width = "%s" force = "%s" />\n' % (segName, parentName, leng, str(geom.y), str(geom.x), str(force)))
+                #addedLines.append("<xacro:segment name=" +segName+" parent="+parentName+" length="+lengthString+" height="+str(geom.y)+" width="+str(geom.x)+" force="+str(force)+ " />\n")
                 jointNames.append(name+"seg"+str(n)+"_"+name+"seg"+str(n+1)+"_joint")
 
     generateYAML("fingers", jointNames)
+    generateSpringYAML("fingerSprings", jointNames)
 
     for c in toRemove:
         root.remove(c)
--- scripts/gripperVisualiser.py
+++ scripts/gripperVisualiser.py
@@ -4,10 +4,12 @@ from Tkinter import *
 from math import *
 import tf2_py
 import tf2_ros
+import roslib
 import rospy, os, time, math
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
 import geometry_msgs.msg
+from gazebo_msgs.msg import ApplyJointEffort
 
 import roslib; roslib.load_manifest('urdfdom_py')
 from urdf_parser_py.urdf import URDF
@@ -40,36 +42,170 @@ class JointLink(object):
         self.previous = None
         self.next = None
         self.link = None
+        self.pos = Vector2(0,0)
+        self.inside = Vector2(0,0)
+        self.outside = Vector2(0,0)
+        self.insideStrain = 0
+        self.outsideStrain = 0
         
-
-    def draw(self, dis):
+    def update(self, jointLinks):
         transform = getTransform("palm", self.link).transform
-        pos = Vector2(transform.translation.y*1000, transform.translation.z*1000)
+        self.pos = Vector2(transform.translation.y*1000, transform.translation.z*1000)
 
-        dir = Vector2(0,0)
+        if(self.previous == None):
+            dir = Vector2(0,1)
+        else:
+            dir = Vector2(0,0)
+            if(self.next):
+                t1 = getTransform("palm", jointLinks[self.next].link).transform
+                p1 = Vector2(t1.translation.y*1000, t1.translation.z*1000)
+                dir += p1-self.pos
 
-        if(self.next):
-            t1 = getTransform("palm", jointLinks[self.next].link).transform
-            p1 = Vector2(t1.translation.y*1000, t1.translation.z*1000)
-            dir += p1-pos
-        if(self.previous):
-            t1 = getTransform("palm", jointLinks[self.previous].link).transform
-            p1 = Vector2(t1.translation.y*1000, t1.translation.z*1000)
-            dir += pos-p1
+            if(self.previous):
+                t1 = getTransform("palm", jointLinks[self.previous].link).transform
+                p1 = Vector2(t1.translation.y*1000, t1.translation.z*1000)
+                
+                dir += self.pos-p1
 
         dir = dir.Normalise()
-        perp = Vector2(dir.y, -dir.x)
+        rot = transform.rotation
+        eulers = quaternionToEuler(rot.x, rot.y, rot.z, rot.w)
 
-        right = pos + perp*10
-        left = pos - perp*10
+        perp = Vector2(dir.y*cos(eulers[2]), -dir.x*cos(eulers[2]))
+        self.inside = self.pos + perp*10
+        self.outside = self.pos - perp*10
+
+    def draw(self, dis):
         #dis.drawLine(p1.x, p1.y, p1.x +dir.x, p1.y+dir.y)
-        dis.drawCircle(pos.x, pos.y, 5)
-        dis.drawCircle(right.x, right.y, 2, fill = "red")
-        dis.drawCircle(left.x, left.y, 2, fill = "green")
+        dis.drawCircle(self.pos.x, self.pos.y, 5)
+        dis.drawCircle(self.outside.x, self.outside.y, 2)
+        dis.drawCircle(self.inside.x, self.inside.y, 2)
 
 
 
-def getTransform(link1, link2, timeout=5):
+
+def getJoint(name):
+    for j in joints:
+        if(j.name == name):
+            return j
+    return False
+
+
+class Gripper(object):
+    def __init__(self):
+        self.jointNames = rospy.get_param("/hand_controller/joints")
+        self.robot = URDF.from_xml_string(rospy.get_param("/robot_description"))
+        self.link_map = self.robot.link_map
+        self.joint_map = self.robot.joint_map
+        self.jointLinks = {}
+        self.fingerBases = []
+
+    def setup(self):
+        #Add joints with corrosponding links
+        for j in self.jointNames:
+
+            self.jointLinks[j] = JointLink(j)
+            self.jointLinks[j].link = self.robot.joint_map[j].child
+
+        #Find each joints previous and next neighbour to complete chain
+        for j in self.jointLinks.keys():
+            link = self.jointLinks[j].link
+            for i in self.jointLinks.keys():
+                if(self.joint_map[i].child == self.joint_map[j].parent):
+                    self.jointLinks[j].previous = i
+                if(self.joint_map[i].parent == link):
+                    self.jointLinks[j].next = i
+            if(self.joint_map[j].parent == "palm"):
+                self.fingerBases.append(j)
+        print(self.fingerBases)
+
+    def calcStrains(self, dis, base):
+        
+        outsideLength = 0
+        insideLength = 0
+        centerLength = 0
+        visited = []
+
+        joint = base
+        next = self.jointLinks[joint].next
+        while next != None:
+            outside = self.jointLinks[next].outside - self.jointLinks[joint].outside
+            inside = self.jointLinks[next].inside - self.jointLinks[joint].inside
+            center = self.jointLinks[next].pos - self.jointLinks[joint].pos
+            outsideLength += outside.Mag()
+            insideLength += inside.Mag()   
+            centerLength += center.Mag()
+            visited.append(joint)
+            joint = next
+            next = self.jointLinks[next].next 
+        
+        outsideFrac = outsideLength/centerLength
+        outsideCol = '#%02x%02x%02x' % (int(outsideFrac*127), 127, int(127/outsideFrac))
+
+        insideFrac = insideLength/centerLength
+        insideCol = '#%02x%02x%02x' % (int(insideFrac*127), 127, int(127/insideFrac))
+
+        for v in visited:
+            p1 = self.jointLinks[v].inside
+            p2 = self.jointLinks[self.jointLinks[v].next].inside
+            dis.drawLine(p1.x, p1.y, p2.x, p2.y, fill=insideCol)
+            p1 = self.jointLinks[v].outside
+            p2 = self.jointLinks[self.jointLinks[v].next].outside
+            dis.drawLine(p1.x, p1.y, p2.x, p2.y, fill=outsideCol)
+
+        return centerLength, outsideFrac, insideFrac
+        
+    def calcCurve(self, dis, base):
+        
+        l1, outsideStrain, insideStrain = self.calcStrains(dis, base)
+        sign = +1
+        if(outsideStrain >= 1):
+            l2 = l1*outsideStrain
+            print("out")
+        else:
+            l2 = l1*insideStrain
+            sign = -1
+            print("in")
+        offset = 10
+        R = (offset*l1)/(l1-l2) * sign
+        theta = l1/R
+        
+        p = getTransform("palm", self.jointLinks[base].link).transform
+        eulers = quaternionToEuler(p.rotation.x, p.rotation.y, p.rotation.z, p.rotation.z)
+        p = Vector2(p.translation.y*1000, p.translation.z*1000)
+        
+
+        c = p + Vector2(-R*cos(eulers[2]), 0)
+        dis.drawCircle(c.x, c.y, 5, fill = "red")
+        end = Vector2(c.x+-R*cos(pi-theta), c.y+R*sin(pi-theta))
+        dis.drawCircle(end.x, end.y, 5, fill="green")
+
+    def applySpring(self, name):
+        print("waiting for service")
+        rospy.wait_for_service('/gazebo/apply_joint_effort')
+        #try:
+        applyEffort = rospy.ServiceProxy('/gazebo/apply_joint_effort', LinkState)
+        data = ApplyJointEffort("{joint_name: %s, effort: 2, start_time: %s, duration: %s}" % (name, 0, 1000000000))
+        applyEffort(data)
+        #except:
+        #    print("error")
+        #    pass
+
+
+    def update(self):
+        for j in self.jointLinks.values():
+            j.update(self.jointLinks)
+        for j in self.jointNames:
+            self.applySpring(j)
+
+    def draw(self, dis):
+        for b in self.fingerBases[1:2]:
+            self.calcCurve(dis, b)
+        #self.calcCurve(dis)
+        #for j in self.jointLinks.values():
+        #    j.draw(dis)
+        
+def getTransform(link1, link2,  timeout=5):
 
             found = False
             while found == False:
@@ -82,18 +218,24 @@ def getTransform(link1, link2, timeout=5):
                     return False
             return trans
 
+def quaternionToEuler(x, y, z, w):
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = atan2(sinr_cosp, cosr_cosp)
 
-def getJoint(name):
-    for j in joints:
-        if(j.name == name):
-            return j
-    return False
-
-
-class Gripper(object):
-    def __init__(self):
-        self.jointLinks = []
-        
+    sinp = 2 * (w * y - z * x)
+    if(abs(sinp) >= 1):
+        if(sinp > 0):
+            pitch = pi/2
+        else:
+            pitch = -pi/2
+    else:
+        pitch = asin(sinp)
+    
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = atan2(siny_cosp, cosy_cosp)
+    return roll, pitch, yaw
 
 root = Tk()
 d = display.Display(root)
@@ -104,6 +246,27 @@ rospy.logerr("controller starting")
 
 tfBuffer = tf2_ros.Buffer()
 listener = tf2_ros.TransformListener(tfBuffer)
+
+g = Gripper()
+g.setup()
+
+print("Setup complete")
+
+while True:
+    d.clear()
+    g.update()
+    #g.calcStrains(d)
+    g.draw(d)
+    d.display()
+    #print("<----------->")
+    #t1 = getTransform("palm", "LTseg3").transform.rotation
+    #print(quaternionToEuler(t1.x, t1.y, t1.z, t1.w))
+    #print("-----------")
+    #t1 = getTransform("palm", "RTseg3").transform.rotation
+    #print(quaternionToEuler(t1.x, t1.y, t1.z, t1.w))
+
+
+
 
 joints = []
 links = []
